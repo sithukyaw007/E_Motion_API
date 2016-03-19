330b5f1da50ad90233a001eeb8ccf2a4576f5061
Sithu <sithukyaw007@gmail.com>
2016-03-20 00:06:55 +0800

    Initial commit for first time

---
+++ client/app/admin/admin.module.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('emotionapiApp.admin', [
+  'emotionapiApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('emotionapiApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
